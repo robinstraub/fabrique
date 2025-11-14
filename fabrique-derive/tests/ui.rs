5858298413efc8ee60d73208e0d69c19990d820d
Robin Straub <robin@straub.pro>
2025-11-14 15:35:11 +0100

    refactor: gate sql related code behind the sqlx feature (#15)

--- fabrique-derive/tests/ui.rs
+++ fabrique-derive/tests/ui.rs
@@ -3,7 +3,10 @@ fn ui() {
     let t = trybuild::TestCases::new();
     t.compile_fail("tests/ui/*.rs");
 
-    // derive_persistable
-    t.pass("tests/ui/persistable/pass/*.rs");
-    t.compile_fail("tests/ui/persistable/fail/*.rs");
+    // derive_persistable (only when sqlx feature is enabled)
+    #[cfg(feature = "sqlx")]
+    {
+        t.pass("tests/ui/persistable/pass/*.rs");
+        t.compile_fail("tests/ui/persistable/fail/*.rs");
+    }
 }
