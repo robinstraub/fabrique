1c2f72af7d70edce05bb8c1442c62cdba94201c3
Robin <robin@straub.pro>
2025-12-14 12:10:29 +0100

    chore: reorganize module documentation

--- fabrique/src/lib.rs
+++ fabrique/src/lib.rs
@@ -1,321 +1,78 @@
-//! # Fabrique: Getting Started
+//! # Fabrique
 //!
 //! Fluent ORM for Rust with type-safe queries and integrated testing tools.
 //!
-//! ## Introduction
+//! Fabrique is an object-relational mapper (ORM) that makes it enjoyable to interact
+//! with your database. It provides a fluent, type-safe API for defining models,
+//! querying data, and generating test fixtures.
 //!
-//! Fabrique is an object-relational mapper (ORM) that makes it enjoyable to
-//! interact with your database. When using Fabrique, each database table has a
-//! corresponding "Model" that is used to interact with that table. In addition
-//! to retrieving records from the database table, Fabrique models allow you to
-//! insert, update, and delete records from the table as well.
+//! ## Core Features
 //!
-//! ## Fabrique Model Conventions
+//! ### Models
 //!
-//! Let's examine a basic model class and discuss some of Fabrique's key
-//! conventions:
+//! Define database models using the `#[derive(Model)]` macro. Models automatically
+//! map to database tables and provide methods for CRUD operations, soft deletes,
+//! and more. See the [`model`] module for detailed documentation.
 //!
-//!```rust,no_run
-//! # use fabrique::prelude::*;
-//! # use uuid::Uuid;
+//! ### Query Builder
 //!
-//! #[derive(Model)]
+//! Build type-safe database queries with a fluent API. The query builder provides
+//! methods for filtering, ordering, limiting results, and more. See the
+//! [`query_builder`] module for details.
+//!
+//! ### Factories
+//!
+//! Generate test data easily with the `#[derive(Factory)]` macro. Factories help
+//! you create model instances for testing without manually specifying every field.
+//! See the [`factory`] module for more information.
+//!
+//! ## Quick Example
+//!
+//! ```rust,no_run
+//! use fabrique::prelude::*;
+//! use sqlx::PgPool;
+//!
+//! // Define a model
+//! #[derive(Model, Factory)]
 //! pub struct Anvil {
 //!     id: uuid::Uuid,
-//!     // --snip--
+//!     weight: i32,
+//!     material: String,
 //! }
-//! ```
 //!
-//! ### Table Names
-//!
-//! After glancing at the example above, you may have noticed that we did not
-//! tell Fabrique which database table corresponds to our Flight model. By
-//! convention, the "snake case", plural name of the class will be used as the
-//! table name unless another name is explicitly specified. So, in this case,
-//! Eloquent will assume the `Anvil` model stores records in the `anvils` table,
-//! while a `RocketShoe` model would store records in a `rocket_shoes` table.
-//!
-//! If your model's corresponding database table does not fit this convention,
-//! you may manually specify the model's table name by defining a table property
-//! on the model:
-//!
-//!```rust,no_run
-//! # use fabrique::prelude::*;
-//! # use uuid::Uuid;
-//!
-//! #[derive(Model)]
-//! #[fabrique(table="acme_anvil")]
-//! pub struct Anvil {
-//!     id: uuid::Uuid,
-//!     // --snip--
-//! }
-//! ```
-//!
-//! ### Primary Keys
-//!
-//! Fabrique will also assume that each model has a primary key column named id.
-//! Otherwise, you must annotate a field with a `fabrique(primary_key)`
-//! attribute to specify which field serves as your model's primary key:
-//!
-//!```rust,no_run
-//! # use fabrique::prelude::*;
-//! # use uuid::Uuid;
-//!
-//! #[derive(Model)]
-//! pub struct Anvil {
-//!     #[fabrique(primary_key)]
-//!     anvil_id: uuid::Uuid,
-//! }
-//! ```
-//!
-//! ### "Composite" Primary Keys
-//!
-//! Fabrique requires each model to have at least one uniquely identifying "ID"
-//! that can serve as its primary key. Unlike some ORMs, "composite" primary
-//! keys are fully supported by Fabrique models through the use of multiple
-//! #[fabrique(primary_key)] attributes:
-//!
-//! ```rust,no_run
-//! # use fabrique::{Model, Persist, Query};
-//! # use uuid::Uuid;
-//!
-//! #[derive(Model)]
-//! pub struct Anvil {
-//!     #[fabrique(primary_key)]
-//!     production_batch: String,
-//!
-//!     #[fabrique(primary_key)]
-//!     sequence_number: i32,
-//!
-//!     // --snip--
-//! }
-//! ```
-//!
-//! ## Retrieving Models
-//!
-//! Once you have created a model and **created its corresponding database
-//! table**  (using your preferred migration tool or SQL), you are ready to
-//! start retrieving data from your database. You can think of each Fabrique
-//! model as a powerful [query builder][QueryBuilder] allowing you to fluently
-//! query the database table associated with the model. The model's
-//! [all][Query::all] method will retrieve all of the records from the
-//! model's associated database table:
-//!
-//! ```rust,no_run
-//! # use fabrique::prelude::*;
-//! # use sqlx::PgPool;
-//! #
-//! # #[derive(Factory, Model)]
-//! # pub struct Anvil {
-//! #     id: uuid::Uuid,
-//! # }
-//! #
-//! # async fn example(connection: &PgPool) -> Result<(), sqlx::Error> {
-//! let anvils: Vec<Anvil> = Anvil::all(&connection).await?;
-//! #     Ok(())
-//! # }
-//! ```
-//!
-//! ### Building Queries
-//!
-//! The Fabrique [all][Query::all] method will return all of the results
-//! in the model's table. However, since each Fabrique model serves as a [query
-//! builder][QueryBuilder], you may add additional constraints to queries and
-//! then invoke the get method to retrieve the results:
-//!
-//! ```rust,no_run
-//! # use fabrique::prelude::*;
-//! # use sqlx::PgPool;
-//! #
-//! # #[derive(Factory, Model)]
-//! # pub struct Anvil {
-//! #     id: uuid::Uuid,
-//! #     weight: i32,
-//! # }
-//! #
-//! # async fn example(connection: &PgPool) -> Result<(), sqlx::Error> {
-//! let anvils: Vec<Anvil> = Anvil::query()
-//!     .r#where(Anvil::WEIGHT, ">=", 42)
-//!     .fetch_all(&connection)
+//! # async fn example(db: &PgPool) -> Result<(), sqlx::Error> {
+//! // Query the database
+//! let heavy_anvils: Vec<Anvil> = Anvil::query()
+//!     .r#where(Anvil::WEIGHT, ">=", 100)
+//!     .fetch_all(db)
 //!     .await?;
-//! #     Ok(())
+//!
+//! // Create a new record
+//! let anvil = Anvil {
+//!     id: uuid::Uuid::new_v4(),
+//!     weight: 150,
+//!     material: "iron".to_string(),
+//! };
+//! anvil.create(db).await?;
+//!
+//! // Generate test data with factories
+//! let test_anvil = Anvil::factory().create(db).await?;
+//! # Ok(())
 //! # }
 //! ```
 //!
-//! ## Inserting and Updating Models
+//! ## Getting Started
 //!
-//! ### Inserts
+//! To use Fabrique in your project, add it to your `Cargo.toml`:
 //!
-//! To insert a new record into the database, you should instantiate a new model
-//! instance and set attributes on the model. Then, call the
-//! [create][Persist::create] method on the model instance:
-//!
-//! ```rust,no_run
-//! # use fabrique::prelude::*;
-//! # use sqlx::PgPool;
-//! #
-//! # #[derive(Factory, Model)]
-//! # pub struct Anvil {
-//! #     id: uuid::Uuid,
-//! # }
-//! #
-//! # async fn example(connection: &PgPool, anvil: Anvil) -> Result<(), sqlx::Error> {
-//! anvil.create(&connection).await?;
-//! #     Ok(())
-//! # }
+//! ```toml
+//! [dependencies]
+//! fabrique = "0.1"
+//! sqlx = { version = "0.8", features = ["runtime-tokio", "postgres"] }
 //! ```
 //!
-//! ## Deleting Models
-//!
-//! To delete a model, you may call the [delete][Persist::delete] method on
-//! the model instance:
-//!
-//! ```rust,no_run
-//! # use fabrique::prelude::*;
-//! # use sqlx::PgPool;
-//! #
-//! # #[derive(Factory, Model)]
-//! # pub struct Anvil {
-//! #     id: uuid::Uuid,
-//! # }
-//! #
-//! # async fn example(connection: &PgPool, anvil: Anvil) -> Result<(), sqlx::Error> {
-//! anvil.delete(&connection).await?;
-//! #     Ok(())
-//! # }
-//! ```
-//!
-//! ### Deleting an Existing Model by its Primary Key
-//!
-//! In the example above, we are retrieving the model from the database before
-//! calling the [delete][Persist::delete] method. However, if you know the
-//! primary key of the model, you may delete the model without explicitly
-//! retrieving it by calling the [destroy][Persist::destroy] method.
-//!
-//! ```rust,no_run
-//! # use fabrique::prelude::*;
-//! # use sqlx::PgPool;
-//! # use uuid::Uuid;
-//! #
-//! # #[derive(Factory, Model)]
-//! # pub struct Anvil {
-//! #     id: uuid::Uuid,
-//! # }
-//! #
-//! # async fn example(connection: &PgPool, id: Uuid) -> Result<(), sqlx::Error> {
-//! Anvil::destroy(&connection, id).await?;
-//! #     Ok(())
-//! # }
-//! ```
-//!
-//! ### Soft Deleting
-//!
-//! In addition to actually removing records from your database, Fabrique can
-//! also "soft delete" models. When models are soft deleted, they are not
-//! actually removed from your database. Instead, a soft delete attribute is set
-//! on the model indicating the date and time at which the model was "deleted".
-//! To enable soft deletes for a model, annotate an attribute with the
-//! `#[fabrique(soft_delete)]` annotation. The attribute type must
-//! be an optional date, such as `Option<chrono::DateTime<chrono::Utc>>`.
-//!
-//!```rust,no_run
-//! # use fabrique::prelude::*;
-//! # use sqlx::PgPool;
-//! # use uuid::Uuid;
-//! use chrono::{DateTime, Utc};
-//!
-//! #[derive(Factory, Model)]
-//! pub struct Anvil {
-//!     id: uuid::Uuid,
-//!
-//!     #[fabrique(soft_delete)]
-//!     deleted_at: Option<DateTime<Utc>>
-//! }
-//! ```
-//!
-//! Now, when you call the delete method on the model, the `deleted_at` column
-//! will be set to the current date and time. However, the model's database
-//! record will be left in the table. When querying a model that uses soft
-//! deletes, the soft deleted models will automatically be excluded from all
-//! query results.
-//!
-//! To determine if a given model instance has been soft deleted, you may use
-//! the [trashed][SoftDelete::trashed] method:
-//!
-//!```rust,no_run
-//! # use fabrique::prelude::*;
-//! # use sqlx::PgPool;
-//! # use uuid::Uuid;
-//! # use chrono::{DateTime, Utc};
-//!
-//! # #[derive(Factory, Model)]
-//! # pub struct Anvil {
-//! #    id: uuid::Uuid,
-//! #
-//! #    #[fabrique(soft_delete)]
-//! #    deleted_at: Option<DateTime<Utc>>
-//! # }
-//! #
-//! # async fn example(connection: &PgPool, anvil: Anvil) -> Result<(), sqlx::Error> {
-//! if anvil.trashed(&connection).await? {
-//!     // --snip--
-//! }
-//! #     Ok(())
-//! # }
-//! ```
-//!
-//! ### Restoring Soft Deleted Models
-//!
-//! Sometimes you may wish to "un-delete" a soft deleted model. To restore a
-//! soft deleted model, you may call the [restore][SoftDelete::restore] method
-//! on a model instance. The restore method will set the model's soft delete
-//! column to null:
-//!
-//!```rust,no_run
-//! # use fabrique::prelude::*;
-//! # use sqlx::PgPool;
-//! # use uuid::Uuid;
-//! # use chrono::{DateTime, Utc};
-//!
-//! # #[derive(Factory, Model)]
-//! # pub struct Anvil {
-//! #    id: uuid::Uuid,
-//! #
-//! #    #[fabrique(soft_delete)]
-//! #    deleted_at: Option<DateTime<Utc>>
-//! # }
-//! #
-//! # async fn example(connection: &PgPool, anvil: Anvil) -> Result<(), sqlx::Error> {
-//! anvil.restore(&connection).await?;
-//! #     Ok(())
-//! # }
-//! ```
-//!
-//! ### Permanently Deleting Models
-//!
-//! Sometimes you may need to truly remove a model from your database. You may
-//! use the [hard_delete][HardDelete::hard_delete] method to permanently remove
-//! a soft deleted model from the database table:
-//!
-//!```rust,no_run
-//! # use fabrique::prelude::*;
-//! # use sqlx::PgPool;
-//! # use uuid::Uuid;
-//! # use chrono::{DateTime, Utc};
-//!
-//! # #[derive(Factory, Model)]
-//! # pub struct Anvil {
-//! #    id: uuid::Uuid,
-//! #
-//! #    #[fabrique(soft_delete)]
-//! #    deleted_at: Option<DateTime<Utc>>
-//! # }
-//! #
-//! # async fn example(connection: &PgPool, anvil: Anvil) -> Result<(), sqlx::Error> {
-//! anvil.hard_delete(&connection).await?;
-//! #     Ok(())
-//! # }
-//! ```
+//! Then define your models and start querying. See the [`model`] module for a
+//! comprehensive guide on model conventions and usage
 pub use database::*;
 pub use factory::*;
 pub use model::*;
--- fabrique/src/model.rs
+++ fabrique/src/model.rs
@@ -1,11 +1,316 @@
-//! Model traits for database operations.
+//! # Fabrique Models
 //!
-//! This module provides the core traits that define model behavior in Fabrique:
-//! - `Model`: Defines model identity with primary key and table name
-//! - `Query`: Provides query building and retrieval operations
-//! - `Persist`: Handles model creation and deletion
-//! - `SoftDelete`: Enables soft deletion functionality
-//! - `HardDelete`: Provides permanent deletion operations
+//! Model traits and conventions for database operations.
+//!
+//! ## Introduction
+//!
+//! When using Fabrique, each database table has a corresponding "Model" that is
+//! used to interact with that table. Models allow you to retrieve, insert, update,
+//! and delete records from the database table.
+//!
+//! ## Model Conventions
+//!
+//! Let's examine a basic model class and discuss some of Fabrique's key conventions:
+//!
+//! ```rust,no_run
+//! # use fabrique::prelude::*;
+//! # use uuid::Uuid;
+//!
+//! #[derive(Model)]
+//! pub struct Anvil {
+//!     id: uuid::Uuid,
+//!     // --snip--
+//! }
+//! ```
+//!
+//! ### Table Names
+//!
+//! After glancing at the example above, you may have noticed that we did not tell
+//! Fabrique which database table corresponds to our `Anvil` model. By convention,
+//! the "snake case", plural name of the struct will be used as the table name
+//! unless another name is explicitly specified. So, in this case, Fabrique will
+//! assume the `Anvil` model stores records in the `anvils` table, while a
+//! `RocketShoe` model would store records in a `rocket_shoes` table.
+//!
+//! If your model's corresponding database table does not fit this convention, you
+//! may manually specify the model's table name by defining a table attribute on
+//! the model:
+//!
+//! ```rust,no_run
+//! # use fabrique::prelude::*;
+//! # use uuid::Uuid;
+//!
+//! #[derive(Model)]
+//! #[fabrique(table = "acme_anvil")]
+//! pub struct Anvil {
+//!     id: uuid::Uuid,
+//!     // --snip--
+//! }
+//! ```
+//!
+//! ### Primary Keys
+//!
+//! Fabrique will also assume that each model has a primary key column named `id`.
+//! Otherwise, you must annotate a field with a `fabrique(primary_key)` attribute
+//! to specify which field serves as your model's primary key:
+//!
+//! ```rust,no_run
+//! # use fabrique::prelude::*;
+//! # use uuid::Uuid;
+//!
+//! #[derive(Model)]
+//! pub struct Anvil {
+//!     #[fabrique(primary_key)]
+//!     anvil_id: uuid::Uuid,
+//! }
+//! ```
+//!
+//! ### "Composite" Primary Keys
+//!
+//! Fabrique requires each model to have at least one uniquely identifying "ID"
+//! that can serve as its primary key. Unlike some ORMs, "composite" primary keys
+//! are fully supported by Fabrique models through the use of multiple
+//! `#[fabrique(primary_key)]` attributes:
+//!
+//! ```rust,no_run
+//! # use fabrique::{Model, Persist, Query};
+//! # use uuid::Uuid;
+//!
+//! #[derive(Model)]
+//! pub struct Anvil {
+//!     #[fabrique(primary_key)]
+//!     production_batch: String,
+//!
+//!     #[fabrique(primary_key)]
+//!     sequence_number: i32,
+//!
+//!     // --snip--
+//! }
+//! ```
+//!
+//! ## Retrieving Models
+//!
+//! Once you have created a model and created its corresponding database table
+//! (using your preferred migration tool or SQL), you are ready to start retrieving
+//! data from your database. You can think of each Fabrique model as a powerful
+//! query builder allowing you to fluently query the database table associated with
+//! the model. The model's [`all`][Query::all] method will retrieve all of the
+//! records from the model's associated database table:
+//!
+//! ```rust,no_run
+//! # use fabrique::prelude::*;
+//! # use sqlx::PgPool;
+//! #
+//! # #[derive(Factory, Model)]
+//! # pub struct Anvil {
+//! #     id: uuid::Uuid,
+//! # }
+//! #
+//! # async fn example(connection: &PgPool) -> Result<(), sqlx::Error> {
+//! let anvils: Vec<Anvil> = Anvil::all(&connection).await?;
+//! #     Ok(())
+//! # }
+//! ```
+//!
+//! ### Building Queries
+//!
+//! The Fabrique [`all`][Query::all] method will return all of the results in the
+//! model's table. However, since each Fabrique model serves as a query builder,
+//! you may add additional constraints to queries and then invoke the get method
+//! to retrieve the results:
+//!
+//! ```rust,no_run
+//! # use fabrique::prelude::*;
+//! # use sqlx::PgPool;
+//! #
+//! # #[derive(Factory, Model)]
+//! # pub struct Anvil {
+//! #     id: uuid::Uuid,
+//! #     weight: i32,
+//! # }
+//! #
+//! # async fn example(connection: &PgPool) -> Result<(), sqlx::Error> {
+//! let anvils: Vec<Anvil> = Anvil::query()
+//!     .r#where(Anvil::WEIGHT, ">=", 42)
+//!     .fetch_all(&connection)
+//!     .await?;
+//! #     Ok(())
+//! # }
+//! ```
+//!
+//! ## Inserting and Updating Models
+//!
+//! ### Inserts
+//!
+//! To insert a new record into the database, you should instantiate a new model
+//! instance and set attributes on the model. Then, call the [`create`][Persist::create]
+//! method on the model instance:
+//!
+//! ```rust,no_run
+//! # use fabrique::prelude::*;
+//! # use sqlx::PgPool;
+//! #
+//! # #[derive(Factory, Model)]
+//! # pub struct Anvil {
+//! #     id: uuid::Uuid,
+//! # }
+//! #
+//! # async fn example(connection: &PgPool, anvil: Anvil) -> Result<(), sqlx::Error> {
+//! anvil.create(&connection).await?;
+//! #     Ok(())
+//! # }
+//! ```
+//!
+//! ## Deleting Models
+//!
+//! To delete a model, you may call the [`delete`][Persist::delete] method on the
+//! model instance:
+//!
+//! ```rust,no_run
+//! # use fabrique::prelude::*;
+//! # use sqlx::PgPool;
+//! #
+//! # #[derive(Factory, Model)]
+//! # pub struct Anvil {
+//! #     id: uuid::Uuid,
+//! # }
+//! #
+//! # async fn example(connection: &PgPool, anvil: Anvil) -> Result<(), sqlx::Error> {
+//! anvil.delete(&connection).await?;
+//! #     Ok(())
+//! # }
+//! ```
+//!
+//! ### Deleting an Existing Model by its Primary Key
+//!
+//! In the example above, we are retrieving the model from the database before
+//! calling the [`delete`][Persist::delete] method. However, if you know the primary
+//! key of the model, you may delete the model without explicitly retrieving it by
+//! calling the [`destroy`][Persist::destroy] method.
+//!
+//! ```rust,no_run
+//! # use fabrique::prelude::*;
+//! # use sqlx::PgPool;
+//! # use uuid::Uuid;
+//! #
+//! # #[derive(Factory, Model)]
+//! # pub struct Anvil {
+//! #     id: uuid::Uuid,
+//! # }
+//! #
+//! # async fn example(connection: &PgPool, id: Uuid) -> Result<(), sqlx::Error> {
+//! Anvil::destroy(&connection, id).await?;
+//! #     Ok(())
+//! # }
+//! ```
+//!
+//! ### Soft Deleting
+//!
+//! In addition to actually removing records from your database, Fabrique can also
+//! "soft delete" models. When models are soft deleted, they are not actually
+//! removed from your database. Instead, a soft delete attribute is set on the
+//! model indicating the date and time at which the model was "deleted". To enable
+//! soft deletes for a model, annotate an attribute with the `#[fabrique(soft_delete)]`
+//! annotation. The attribute type must be an optional date, such as
+//! `Option<chrono::DateTime<chrono::Utc>>`.
+//!
+//! ```rust,no_run
+//! # use fabrique::prelude::*;
+//! # use sqlx::PgPool;
+//! # use uuid::Uuid;
+//! use chrono::{DateTime, Utc};
+//!
+//! #[derive(Factory, Model)]
+//! pub struct Anvil {
+//!     id: uuid::Uuid,
+//!
+//!     #[fabrique(soft_delete)]
+//!     deleted_at: Option<DateTime<Utc>>
+//! }
+//! ```
+//!
+//! Now, when you call the delete method on the model, the `deleted_at` column will
+//! be set to the current date and time. However, the model's database record will
+//! be left in the table. When querying a model that uses soft deletes, the soft
+//! deleted models will automatically be excluded from all query results.
+//!
+//! To determine if a given model instance has been soft deleted, you may use the
+//! [`trashed`][SoftDelete::trashed] method:
+//!
+//! ```rust,no_run
+//! # use fabrique::prelude::*;
+//! # use sqlx::PgPool;
+//! # use uuid::Uuid;
+//! # use chrono::{DateTime, Utc};
+//!
+//! # #[derive(Factory, Model)]
+//! # pub struct Anvil {
+//! #    id: uuid::Uuid,
+//! #
+//! #    #[fabrique(soft_delete)]
+//! #    deleted_at: Option<DateTime<Utc>>
+//! # }
+//! #
+//! # async fn example(connection: &PgPool, anvil: Anvil) -> Result<(), sqlx::Error> {
+//! if anvil.trashed(&connection).await? {
+//!     // --snip--
+//! }
+//! #     Ok(())
+//! # }
+//! ```
+//!
+//! ### Restoring Soft Deleted Models
+//!
+//! Sometimes you may wish to "un-delete" a soft deleted model. To restore a soft
+//! deleted model, you may call the [`restore`][SoftDelete::restore] method on a
+//! model instance. The restore method will set the model's soft delete column to
+//! null:
+//!
+//! ```rust,no_run
+//! # use fabrique::prelude::*;
+//! # use sqlx::PgPool;
+//! # use uuid::Uuid;
+//! # use chrono::{DateTime, Utc};
+//!
+//! # #[derive(Factory, Model)]
+//! # pub struct Anvil {
+//! #    id: uuid::Uuid,
+//! #
+//! #    #[fabrique(soft_delete)]
+//! #    deleted_at: Option<DateTime<Utc>>
+//! # }
+//! #
+//! # async fn example(connection: &PgPool, anvil: Anvil) -> Result<(), sqlx::Error> {
+//! anvil.restore(&connection).await?;
+//! #     Ok(())
+//! # }
+//! ```
+//!
+//! ### Permanently Deleting Models
+//!
+//! Sometimes you may need to truly remove a model from your database. You may use
+//! the [`hard_delete`][HardDelete::hard_delete] method to permanently remove a
+//! soft deleted model from the database table:
+//!
+//! ```rust,no_run
+//! # use fabrique::prelude::*;
+//! # use sqlx::PgPool;
+//! # use uuid::Uuid;
+//! # use chrono::{DateTime, Utc};
+//!
+//! # #[derive(Factory, Model)]
+//! # pub struct Anvil {
+//! #    id: uuid::Uuid,
+//! #
+//! #    #[fabrique(soft_delete)]
+//! #    deleted_at: Option<DateTime<Utc>>
+//! # }
+//! #
+//! # async fn example(connection: &PgPool, anvil: Anvil) -> Result<(), sqlx::Error> {
+//! anvil.hard_delete(&connection).await?;
+//! #     Ok(())
+//! # }
+//! ```
 
 pub use fabrique_core::model::*;
 pub use fabrique_derive::Model;
